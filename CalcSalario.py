3a61390eb27b82ddd12cd60ce424b6174e0b15ff
SarahdeMoraes <sarah.batista@germinare.org.br>
2024-06-20 09:52:40 -0300

    Subindo arquivo .py

---
+++ CalcSalario.py
@@ -0,0 +1,7 @@
+import datetime
+
+anoContratado = int(input("Qual foi o ano de contratação? "))
+salarioInicial = float(input("Qual seria o salario inicial do funcionario? "))
+
+anosTrabalhados = datetime.date.today().year
+
